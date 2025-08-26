cae13131db38f5a5bcbca469a4fe541841fabbf8
Muthulakshmi <muthulakshmi5293@gmail.com>
2025-08-27 00:35:27 +0530

    Update settings.py

--- bookstoreproject/settings.py
+++ bookstoreproject/settings.py
@@ -114,8 +114,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+import os
+from pathlib import Path
 
-STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Static files (CSS, JS, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
